62a09595cf4d3953c6d42b6003c518ec17365b35
Irina <sviderskaya@yandex.ru>
2022-11-12 14:58:26 +0300

    Задаем условие выполнения задачи

--- Program.cs
+++ Program.cs
@@ -3,3 +3,15 @@
 string[] array1 = new string[5] {"1234", "1567", "-2", "computer", "res"};
 string[] array2 = new string[array1.Length];
 int count = 0;
+int count = 0;
+void ArrayIF(string[] array1, string[] array2)
+{
+ int count = 0;
+ for (int i = 0; i < array1.Length; i++)
+ { if(array1[i].Length <= 3)
+ {
+ array2[count] = array1[i];
+ count++;
+ }
+ }
+}
